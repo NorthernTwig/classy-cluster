b7606cbf0fa914dbf59084f01f0e898b4f29bcdd
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-11-24 21:50:02 +0100

    Writes all collected wiki data

--- src/main/java/classyCluster/utils/FileHandler.java
+++ src/main/java/classyCluster/utils/FileHandler.java
@@ -7,6 +7,7 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -18,29 +19,11 @@ public class FileHandler {
         return Files.readAllLines(Paths.get(uri), Charset.defaultCharset());
     }
 
-    public void generateWikiData() throws IOException {
-        HashMap<String, Integer> words = new HashMap<>();
-        List<Path> allFiles = readAll();
-        for (Path file : allFiles) {
-            System.out.println(file.toString());
-            Files.lines(file).forEach(line -> {
-                for (String word: line.split(" ")) {
-                    if (words.containsKey(word)) {
-                        int occurance = words.get(word);
-                        words.replace(word, occurance + 1);
-                    } else {
-                        words.put(word, 1);
-                    }
-                }
-            });
-        }
-        words.forEach((value, key) -> {
-            System.out.println(value);
-            System.out.println(key);
-        });
+    public void writeWiki(String wiki) throws IOException {
+        Files.write(Paths.get("src/main/resources/wikidata.txt"), wiki.getBytes());
     }
 
-    private List<Path> readAll() throws IOException {
+    public List<Path> readAll() throws IOException {
         return Files.walk(Paths.get("src/main/resources/words"))
                 .filter(Files::isRegularFile)
                 .collect(Collectors.toList());
